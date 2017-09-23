303735d6cf48e77f7a5a95cd22ace98ee8e6107a
Grzegorz (Greg) Haranczyk <gzharanczyk@gmail.com>
2017-09-23 13:04:03 -0400

    Create Quiz.swift

---
+++ NYC_Quiz_App/Model/Quiz.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Quiz {
+    
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, answer: Bool) {
+        questionText = text
+        self.answer = answer
+    }
+    
+}
